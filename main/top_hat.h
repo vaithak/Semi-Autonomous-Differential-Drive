6711dee8ac4087633b00d96b8780b70de86c5ad5
c-y-i <caoyi@seas.upenn.edu>
2024-12-14 18:24:10 -0500

    pin change #2

--- main/top_hat.h
+++ main/top_hat.h
@@ -4,8 +4,8 @@
 #include <Wire.h>
 
 #define TOP_HAT_I2C_ADDRESS 0x28
-#define TOP_HAT_SDA 34
-#define TOP_HAT_SCL 7
+#define TOP_HAT_SDA 38
+#define TOP_HAT_SCL 37
 #define TOP_HAT_I2C_FREQUENCY 40000
 #define TOP_HAT_READ_INTERVAL 500
 
